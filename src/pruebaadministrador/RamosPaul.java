ad0d624ab06351643166165ebd6642c6fd89e2f7
14-LAB6 <14-LAB6@PC14-LAB6>
2016-11-09 12:16:43 -0500

    kkkkkk

---
+++ src/pruebaadministrador/RamosPaul.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaadministrador;
+
+/**
+ *
+ * @author 14-LAB6
+ */
+public class RamosPaul {
+    //hola1
+    //hola2
+    
+}
